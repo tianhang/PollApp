3b65a39209e444a634463729f0c95b1d6c3f5a90
William Zhang <tianhang1991@126.com>
2016-05-22 21:07:07 +0800

    add

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import url
 
 from . import views
+from .views import IndexView,DetailView,ResultsView
 
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
