8081451cd8836beb2e195b1c16c129a6a3ecc9cd
William Zhang <tianhang1991@126.com>
2016-05-25 15:54:53 +0800

    add style

--- PollApp/wsgi.py
+++ PollApp/wsgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PollApp.settings")
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PollApp.settings")
-
 application = get_wsgi_application()
+
+
